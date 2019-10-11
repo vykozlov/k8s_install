8fc846566194030a39ab5b66ddd9c9a969c6bc35
valentin.kozlov <valentin.kozlov@kit.edu>
2019-10-11 11:56:06 +0200

    add installation of a node

--- k8s-install_node.sh
+++ k8s-install_node.sh
@@ -56,6 +56,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
    sudo systemctl enable docker && sudo systemctl start docker
 fi
 
+echo ""
+echo "[INFO] Check Docker cgroup"
+echo "  Important for --cgroup-driver=systemd in /var/lib/kubelet/kubeadm-flags.env"
+echo "  systemd has to be also in /usr/lib/systemd/system/docker.service"
+DOCKER_CGROUP=$(sudo docker info |grep -i cgroup)
+[[ $DOCKER_CGROUP != *"systemd"* ]] && sudo yum install -y nano && sudo nano /usr/lib/systemd/system/docker.service
+
 echo "[INFO] Load ip_tables"
 sudo modprobe ip_tables
 echo -n "[!] Enable bridge-nf-call-iptables (needed)? [y/n] "
@@ -67,12 +74,6 @@ fi
 
 echo "[INFO] Checking bridge-nf-call-iptables (has to be 1)"
 cat /proc/sys/net/bridge/bridge-nf-call-iptables
-
-echo ""
-echo "[INFO] Check Docker cgroup"
-echo "  Important for KUBELET_CGROUP_ARGS=--cgroup-driver=systemd"
-echo "  in /etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
-sudo docker info |grep -i cgroup
 
 echo ""
 echo "[INFO] Install kubelet, kubadm, kubectl"
@@ -91,40 +92,8 @@ sudo setenforce 0
 sudo yum install -y kubelet-$K8S_VER kubeadm-$K8S_VER kubectl-$K8S_VER
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 
-echo -n "[!] Init master? [y/n] "
-read REPLY
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-   sudo kubeadm init --pod-network-cidr=$K8S_POD_NETWORK
-fi
-
-echo -n "[!] Copy admin.conf to $USER account? [y/n] "
-read REPLY
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-   mkdir -p $HOME/.kube
-   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-   sudo chown $(id -u):$(id -g) $HOME/.kube/config
-fi
-
-echo -n "[!] Install Flannel network? [y/n] "
-read REPLY
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-   echo "..[INFO] Installing Flannel"
-   sudo bash -c 'export KUBECONFIG=/etc/kubernetes/admin.conf && \
-   kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml'
-fi
-
-echo "[INFO] Checking pods of kubernetes"
-kubectl get pods --all-namespaces
-
 echo ""
 echo "====================================================================================================="
-echo "[INFO] If all finished with success, go to a node and install k8s and join the node with the cluster:"
-echo "   1. Check that swap is off  (cat /proc/swaps). If not: sudo swapoff -a"
-echo "   2. Check that 'sudo docker info |grep -i cgroup' returns systemd, if not edit:"
-echo "      /usr/lib/systemd/system/docker.service :  ExecStart=/usr/bin/dockerd  --exec-opt native.cgroupdriver=systemd"
-echo "   3. sudo yum install -y kubelet-$K8S_VER kubeadm-$K8S_VER kubectl-$K8S_VER"
-echo "   4. run kubeadm join ... (see previous print-outs after kubeadm init on the master)"
+echo "[INFO] If all finished with success:"
+echo "   - run kubeadm join ... (see print-outs after kubeadm init on the master)"
 echo "====================================================================================================="
